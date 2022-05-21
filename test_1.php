85b613fd87e853d116e37ef2fa14d6606cae5b39
jatloe <85522239+jatloe@users.noreply.github.com>
2022-05-21 18:04:54 -0400

    Create test_1.php

---
+++ test_1.php
@@ -0,0 +1,4 @@
+<?php
+  //show the data that was received from the client along with the request
+  print($_GET['wonderful_data']); //outputs whatever the user entered into the text input of the form
+?>
